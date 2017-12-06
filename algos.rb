70d0eab67b9a544135e041fd044397533dfff837
John  Lee <johnlee@johns-mbp.lan>
2017-12-05 18:29:33 -0800

    add fizz_buzz

--- algos.rb
+++ algos.rb
@@ -13,3 +13,20 @@ end
 
 # reverse_string("This is a test!")
 # => "Test a is this!""
+
+def fizz_buzz(n)
+  i = 1
+  while i <= n
+    if i % 15 == 0 
+      puts "fizz buzz" 
+    elsif i % 5 == 0 
+      puts "buzz"
+    elsif i % 3 == 0 
+      puts "fizz"
+    else
+      puts i
+    end
+    i+=1
+  end
+end
+
